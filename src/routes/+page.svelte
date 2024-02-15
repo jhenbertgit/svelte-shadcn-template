0409ec71dd392ac006b34b9c7642c59b0ec5cba1
Jhenbert Villamucho <jvv205756@gmail.com>
2024-02-15 19:48:21 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1 class="font-bold text-2xl text-center mt-7">Welcome to SvelteKit</h1>
+
+<div class="flex flex-col items-center mt-7">
+  <Button>Test Click</Button>
+</div>
