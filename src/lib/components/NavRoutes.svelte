833eb6047e0627e0bc784e7c02afbbad9fc88262
Jhenbert Villamucho <jvv205756@gmail.com>
2024-02-18 17:52:13 +0800

    move type Route to app.d.ts

--- src/lib/components/NavRoutes.svelte
+++ src/lib/components/NavRoutes.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  type Routes = {
-    label: string;
-    url: string;
-  };
 
   export let routes: Routes[] = [];
 
   $: currentRoute = $page.url.pathname;
-
-  // const isActive = (route: string) => {
-  //   return route === currentRoute;
-  // };
 </script>
 
 {#each routes as route}
